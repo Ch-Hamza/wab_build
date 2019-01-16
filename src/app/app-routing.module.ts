6f187400f398ee0c3c667392e6a981bf62baef82
walimkr <islemmseddi@gmail.com>
2019-01-16 12:49:23 +0100

    fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { CabinetComponent } from 'src/app/cabinet/cabinet.component';
 import { AccueilComponent } from 'src/app/accueil/accueil.component';
 import { ActualiteComponent } from 'src/app/actualite/actualite.component';
+import {ActualitesComponent} from './actualites/actualites.component';
+import {ServicesComponent} from './services/services.component';
 const routes: Routes = [
   
   {path : 'cabinet' , component : CabinetComponent} ,
   {path : '' , component : AccueilComponent},
-  {path : 'actualite' , component : ActualiteComponent}
+  {path : 'actualites' , component : ActualitesComponent},
+  {path : 'actualites/actualite1' , component : ActualiteComponent},
+  {path : 'services' , component : ServicesComponent}
 
 
    

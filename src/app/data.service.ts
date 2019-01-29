68cacfbfeb66ba4c1f72921408e5108240cde407
Dridi Lina <lina.dridi.97@gmail.com>
2019-01-29 23:20:09 +0100

    contact form back fix

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { getLocaleDateFormat } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
@@ -26,6 +27,18 @@ export class DataService {
   getPublication(id) {
     console.log(id);
     return this.Http.get('http://localhost/Back/get_pub_by_id.php?id=' + id);
+  }
+  addMessage(obj) {
+
+    let body = new URLSearchParams();
+    body.set('lastName', obj.lastName);
+    body.set('firstName', obj.firstName);
+    body.set('phone', obj.phone);
+    body.set('email', obj.email);
+    body.set('subject', obj.subject);
+    body.set('message', obj.message);
+    let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+    return this.Http.post<string>('http://localhost/Back/get_responseContact.php', body.toString(), { headers, responseType: "text" as 'json' });
   }
 
   isLogged(data: any) {

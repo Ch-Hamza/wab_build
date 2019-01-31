cf47869bb0e595c55c44721b605fd7029e77ae9b
walimkr <islemmseddi@gmail.com>
2019-01-31 17:20:48 +0100

    pagination

--- src/app/actualites/actualites.component.ts
+++ src/app/actualites/actualites.component.ts
@@ -21,13 +21,33 @@ import {
 })
 export class ActualitesComponent implements OnInit {
   actualites;
+  pages=[];
+  n;
   constructor(private router: Router, private dataService: DataService, private app: AppComponent) {
     app.EspaceAdmin = false;
-    this.dataService.getActualites().subscribe(res => {
+      this.dataService.getActualitesnum().subscribe(res => {
+        if(res[0]['COUNT(id)']%6==0){
+         this.n= res[0]['COUNT(id)']/6;}
+        else this.n= res[0]['COUNT(id)']/6+1;
+          for(let i=1;i<=this.n;i++){
+            this.pages.push(i);
+          }
+          console.log(this.pages);
+      });
+    this.dataService.getActualitespage(1).subscribe(res => {
       this.actualites = res;
+      this.actualites.reverse();
       console.log(this.actualites);
     });
   }
 
   ngOnInit() { }
+    getact(i){
+        this.dataService.getActualitespage(i).subscribe(res => {
+            this.actualites = res;
+            this.actualites.reverse();
+            console.log(this.actualites);
+        });
+    }
 }
+

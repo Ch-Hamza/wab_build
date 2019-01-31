772d0e447a6aabfb230fc41dc606b04634edaca2
walimkr <islemmseddi@gmail.com>
2019-01-31 18:23:01 +0100

    fix pagination

--- src/app/actualites/actualites.component.ts
+++ src/app/actualites/actualites.component.ts
@@ -23,13 +23,18 @@ export class ActualitesComponent implements OnInit {
   actualites;
   pages=[];
   n;
+  nb;
   constructor(private router: Router, private dataService: DataService, private app: AppComponent) {
     app.EspaceAdmin = false;
       this.dataService.getActualitesnum().subscribe(res => {
         if(res[0]['COUNT(id)']%6==0){
          this.n= res[0]['COUNT(id)']/6;}
-        else this.n= res[0]['COUNT(id)']/6+1;
-          for(let i=1;i<=this.n;i++){
+        else
+          {this.n= parseInt((res[0]['COUNT(id)']/6)+1);
+              console.log(this.n);}
+        if(this.n>5){
+          this.nb=5;}
+          for(let i=1;i<=this.nb;i++){
             this.pages.push(i);
           }
           console.log(this.pages);
@@ -43,11 +48,19 @@ export class ActualitesComponent implements OnInit {
 
   ngOnInit() { }
     getact(i){
-        this.dataService.getActualitespage(i).subscribe(res => {
+    this.dataService.getActualitespage(i).subscribe(res => {
             this.actualites = res;
             this.actualites.reverse();
             console.log(this.actualites);
         });
+    this.pages=[];
+        if(this.n-i>=5){
+            this.nb=i+5;}
+            else{this.nb=this.n;}
+            if(i>1){i=i-1;}
+        for(i;i<=this.nb;i++){
+            this.pages.push(i);
+        }
     }
 }
 

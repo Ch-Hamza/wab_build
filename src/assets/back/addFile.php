7377da67fab38d406aac90652bf62aed6c573cf7
chaymaBD <chayma.dhafer@gmail.com>
2019-02-03 20:00:19 +0100

    fix

--- src/assets/back/addFile.php
+++ src/assets/back/addFile.php
@@ -1,11 +1,63 @@
 <?php
+/*
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+echo "this file ";
+include "Crud.php"; 
+$crud= new Crud();
+ 
+
+
+//read received JSON (you can receive by $_POST if you want, just change this line)
+     //   $data = file_get_contents("php://input");
+//parse JSON to PHP object
+      //  $data = json_decode($data); 
+        echo "this file11 ";
+     $_POST = json_decode(file_get_contents('php://input'));
+   
+var_dump(  $_POST);
+echo "this file22 ";
+
+//return your service response
+        $jsonResponse = "{ \"response\": true, \"text\": \"Hey, your name is: " .$_POST->name . "\"}";        
+        echo $jsonResponse;
+        exit;
+
+
+
+*/
+
+
+        //third version
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+echo "this file ";
+/*
+var_dump($_FILES);
+var_dump($_POST);
+foreach($_POST as $key=>$value)
+{
+  var_dump($key);
+}
+
+ //$_POST = json_decode(file_get_contents('php://input'));
+
+
+*/
+
+
+
+
+
+
+/*header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 include "Crud.php"; 
 $crud= new Crud();
-$_POST = json_decode(file_get_contents('php://input'));
-
-
+$_POST = json_decode(file_get_contents(+));
+echo "helloooooooo";
 $target_dir = "../images";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
@@ -47,5 +99,11 @@ if ($uploadOk == 0) {
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
-}
+}*/
 ?>
+
+
+
+
+
+

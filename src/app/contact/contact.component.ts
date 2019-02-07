e566f34438ecf60aed7d7121b630d6da34068fa6
Dridi Lina <lina.dridi.97@gmail.com>
2019-02-07 02:10:30 +0100

    dynamic contact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '../app.component';
 import { DataService } from '../data.service';
-import {
-  Router
-} from '@angular/router';
+import { Router } from '@angular/router';
+import { delay } from 'q';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -18,11 +17,13 @@ export class ContactComponent implements OnInit {
     console.log(this.model)
     this.dataService.addMessage(this.model).subscribe(res => { console.log(res) });
   }
-
+  contact;
   constructor(private router: Router, private dataService: DataService, private app: AppComponent) {
     app.EspaceAdmin = false;
-
-
+    this.dataService.getContact().subscribe(res => {
+      this.contact = res;
+      console.log(this.contact);
+    });
   }
 
 

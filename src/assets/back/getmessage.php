caa448eb2ba10f1548561bb24d4b4f0a4048c8af
Jihene HANI <jihene.el.hani.17@gmail.com>
2019-02-09 21:38:51 +0100

    Jihen- back- Affichage de chaque élément du tableau - pub actu message

---
+++ src/assets/back/getmessage.php
@@ -0,0 +1,11 @@
+<?php
+
+    include "Crud.php";
+    $crud= new Crud();
+    $query="SELECT  `id`, `nom`, `prenom`, `telephone`, `email`, `sujet`, `messagee`  FROM contact_form";
+        
+        $data=$crud->getData($query);
+         echo json_encode($data);
+    
+
+?>

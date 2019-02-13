c11587a03b62830cbbd409bd3c60bf9cb076fd13
Dridi Lina <lina.dridi.97@gmail.com>
2019-02-13 21:54:39 +0100

    merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule ,CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA} from '@angular/core';
-import {StorageServiceModule} from 'angular-webstorage-service';
-import{MDBBootstrapModule} from 'angular-bootstrap-md';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { StorageServiceModule } from 'angular-webstorage-service';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 //import {NgxImageZoomModule} from 'ngx-image-zoom';
 import { Ng2UploaderModule } from 'ng2-uploader';
 
@@ -17,9 +17,10 @@ import { AccueilComponent } from './accueil/accueil.component';
 import { LoginComponent } from './login/login.component';
 import { PublicationComponent } from './publication/publication.component';
 import { ContactComponent } from './contact/contact.component';
-import { FormsModule ,ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import {DataService} from './data.service';
+//import { HttpClientModule } from 'ngx-http-client';
+import { DataService } from './data.service';
 import { ModifActualiteComponent } from './modif-actualite/modif-actualite.component';
 import { ModifPublicationComponent } from './modif-publication/modif-publication.component';
 import { ModifContactUsComponent } from './modif-contact-us/modif-contact-us.component';
@@ -27,11 +28,11 @@ import { HeaderAdminComponent } from './header-admin/header-admin.component';
 import { BoiteDeReceptionComponent } from './boite-de-reception/boite-de-reception.component';
 import { ModifierUnePublicationComponent } from './modifier-une-publication/modifier-une-publication.component';
 import { ModifierUneActualiteComponent } from './modifier-une-actualite/modifier-une-actualite.component';
- 
+
 import { GardServiceService } from './gard-service.service';
 import { AffichUneActualiteAdminComponent } from './affich-une-actualite-admin/affich-une-actualite-admin.component';
 import { AffichUnePublicationAdminComponent } from './affich-une-publication-admin/affich-une-publication-admin.component';
-import { AffichUnMessageAdminComponent } from './affich-un-message-admin/affich-un-message-admin.component'; 
+import { AffichUnMessageAdminComponent } from './affich-un-message-admin/affich-un-message-admin.component';
 
 @NgModule({
   declarations: [
@@ -55,10 +56,10 @@ import { AffichUnMessageAdminComponent } from './affich-un-message-admin/affich-
     ModifierUneActualiteComponent,
     AffichUneActualiteAdminComponent,
     AffichUnePublicationAdminComponent,
-    AffichUnMessageAdminComponent 
+    AffichUnMessageAdminComponent
   ],
   imports: [
-     
+
     BrowserModule,
     AppRoutingModule,
     FormsModule,
@@ -67,12 +68,12 @@ import { AffichUnMessageAdminComponent } from './affich-un-message-admin/affich-
     ReactiveFormsModule, 
     StorageServiceModule,
     MDBBootstrapModule.forRoot()
-   // NgxImageZoomModule.forRoot()
-   
-    
+    // NgxImageZoomModule.forRoot()
+
+
   ],
-  schemas : [NO_ERRORS_SCHEMA],
-  providers: [  GardServiceService,DataService ],
+  schemas: [NO_ERRORS_SCHEMA],
+  providers: [GardServiceService, DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

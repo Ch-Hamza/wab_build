db306b9aba15312d5be03e27d9c9988d377f90df
Jihene HANI <jihene.el.hani.17@gmail.com>
2019-02-15 02:12:41 +0100

    Jihen- Upload Image

--- src/app/modif-actualite/modif-actualite.component.ts
+++ src/app/modif-actualite/modif-actualite.component.ts
@@ -11,6 +11,7 @@ import {
 import {
   delay
 } from 'q';
+import {HttpClient, HttpClientModule, HttpResponse, HttpEventType, HttpHeaders, HttpParams} from '@angular/common/http';   
 
 @Component({
   selector: 'app-modif-actualite',
@@ -25,7 +26,7 @@ selectedFile: File
     pages=[];
     n;
     nb;
-  constructor(private router: Router, private dataService: DataService,private app:AppComponent  ) {
+  constructor(private Http: HttpClient ,private router: Router, private dataService: DataService,private app:AppComponent  ) {
       app.EspaceAdmin=true;
       this.dataService.getActualitesnum().subscribe(res => {
           if(res[0]['COUNT(id)']%6==0){
@@ -126,7 +127,31 @@ formOff(){
      
   }}
 
+  fileChange(event) {
+   
 
+    let fileList: FileList = event.target.files;
+    if(fileList.length > 0) {
+        let file: File = fileList[0];
+        let formData:FormData = new FormData();
+        formData.append('uploadFile', file, file.name);
+        let headers = new HttpHeaders();
+        /** In Angular 5, including the header Content-Type can invalidate your request */
+         headers.append('Content-Type', 'multipart/form-data');
+        headers.append('Accept', 'application/json');
+      //  let options = new RequestOptions({ headers: headers });
+        const options = {
+          params: new HttpParams(),
+          headers:  headers
+        };
+        this.Http.post(`http://localhost/Back/uploadImage.php`, formData, options)
+            
+            .subscribe(
+                data => console.log('success'),
+                error => console.log(error)
+            )
+    }
+} 
 
 
 }

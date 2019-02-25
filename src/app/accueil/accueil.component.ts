62ff8726888a45f5a2b8fa8ff345f398d8262fcd
Dridi Lina <lina.dridi.97@gmail.com>
2019-02-25 13:22:27 +0100

    fix accueil (chayma)

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -5,7 +5,7 @@ import {
 import {
   DataService
 } from '../data.service';
-import {AppComponent} from '../app.component';
+import { AppComponent } from '../app.component';
 import {
   Observable
 } from 'rxjs';
@@ -18,19 +18,26 @@ import {
 export class AccueilComponent implements OnInit {
   $posts: any[] = [];
   $actual: any[] = [];
-  actualites :any[]=[];
-  constructor(private dataserv: DataService,private app:AppComponent) {
-  app.EspaceAdmin=false;
+  actualites: any[] = [];
+  constructor(private dataserv: DataService, private app: AppComponent) {
+    app.EspaceAdmin = false;
 
 
     this.dataserv.getData('http://localhost/Back/publication.php').subscribe( //url back 
       (data: any) => {
-        for (var i = 0; i < 4; i++) { //retour back les 4 plus récents 
-          this.$posts.push(data[i]);
-
+        var a = data.length;
+        if (a < 5) {
+          this.$posts = data;
         }
 
-        console.log("coucou",this.$posts);
+        else {
+          for (var i = a - 4; i < a; i++) { //retour back les 4 plus récents 
+            this.$posts.push(data[i]);
+
+          }
+
+        }
+        console.log("coucou", this.$posts);
 
       },
 
@@ -40,23 +47,23 @@ export class AccueilComponent implements OnInit {
 
     )
 
-    this.dataserv.getActualites().subscribe((res : any ) => {
-    
+    this.dataserv.getActualites().subscribe((res: any) => {
+
       var a = res.length;
-      if (a<4) {
-        this.actualites = res ;
+      if (a < 4) {
+        this.actualites = res;
       }
       else {
-        for (var i =a-3; i < a; i++) { //retour back les 4 plus récents 
+        for (var i = a - 3; i < a; i++) { //retour back les 4 plus récents 
           this.actualites.push(res[i]);
 
         }
 
-       }
-       console.log(res.length);
-       
+      }
+      console.log(res.length);
+
       console.log(this.actualites);
-  });
+    });
 
 
 

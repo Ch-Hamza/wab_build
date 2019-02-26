3428733892bc885e6a1e8a6fc794e856097a40ec
walimkr <islemmseddi@gmail.com>
2019-02-26 15:28:40 +0100

    fix

--- src/assets/back/getmessage.php
+++ src/assets/back/getmessage.php
@@ -2,7 +2,8 @@
 
     include "Crud.php";
     $crud= new Crud();
-    $query="SELECT  `id`, `nom`, `prenom`, `telephone`, `email`, `sujet`, `messagee`  FROM contact_form";
+    $id=$_GET['id'];
+    $query="SELECT  `id`, `nom`, `prenom`, `telephone`, `email`, `sujet`, `messagee`  FROM contact_form where id='$id'";
         
         $data=$crud->getData($query);
          echo json_encode($data);
